579847138532d8905defd4effd25848f6095b5b6
Anya <hannakilpatrick@gmail.com>
2018-06-16 23:28:37 -0400

    created parallax component

---
+++ client/src/components/Parallax/Parallax.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "./../Form";
+
+const Parallax = () => (
+    <div className="parallax-container">
+        <div className="parallax">
+            <img alt="nytimes building" src="./images/nytimes.jpg"/>
+        </div>
+        <Form/>
+    </div>
+)
+
+export default Parallax;
