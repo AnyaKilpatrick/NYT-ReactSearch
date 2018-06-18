6659b13763e338af24c4ce16f04c4f98d81eca41
Anya <hannakilpatrick@gmail.com>
2018-06-18 00:33:22 -0400

    API request call

---
+++ client/src/utils/API.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const BASEURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+const APIKEY = "&api-key=90c630d1090a40f58564383b995a103d"
+
+export default {
+ //search articles (NYT API)
+ search: function(query, startYear, endYear){
+     return axios.get(BASEURL + "q="+ query +"&begin_date="+startYear+"0101&end_date"+endYear +"0101"+ APIKEY);
+ }
+}
