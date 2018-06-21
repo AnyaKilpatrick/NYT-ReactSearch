823e3126351f4e800ae250ff6870dd3ef324822d
Anya <hannakilpatrick@gmail.com>
2018-06-20 22:58:53 -0400

    displaying saved articles & added btn to delete article

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import './App.css';
 import Home from "./pages/Home";
+import Saved from "./pages/Saved";
 
 
 const App = () => (
  <Router>
     <div className="wrap">
       <Route exact path="/" component={Home}/>
+      <Route exact path="/saved" component={Saved}/>
     </div>
   </Router>
 )
---
+++ client/src/pages/Saved/Saved.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import Header from "./../../components/Header";
+import Results from "./../../components/Results";
+import API from "./../../utils/API";
+import Moment from "moment";
+import { Link } from "react-router-dom";
+
+
+class Saved extends Component {
+    state = {
+        savedArticles: []
+    }
+
+    componentDidMount(){
+        this.loadArticles();
+    }
+
+    loadArticles = () => {
+        API.loadArticles()
+        .then((res)=> {
+            console.log("articles from db" + JSON.stringify(res));
+            this.setState({savedArticles: res.data});
+        })
+        .catch((err)=>console.log(err));
+    }
+
+    deleteArticle = event => {
+        const id = event.target.id;
+        console.log("btn id "+ id);
+        API.deleteArticle(id)
+        .then((res)=>{
+            console.log("article was deleted");
+            this.loadArticles();
+        })
+        .catch((err)=> console.log(err));
+    }
+
+
+    render(){
+    return (
+         <div>
+            <Header/>
+            <h1 id="savedHeader" className="center">Saved Articles</h1>
+            <div className="container">
+            {this.state.savedArticles.map((article, index)=>
+                <Results
+                key={index}
+                id={article._id}
+                headline={article.title}
+                pTag={article.byline}
+                pubDate = {Moment(article.pubDate, "YYYY-MM-DD HH:mm Z").format("lll")}
+                link={article.link}
+                btnAction = {this.deleteArticle}
+                btnText = "Delete"
+                />
+            )}
+            </div>
+        </div>
+        )
+    }
+};
+
+export default Saved;
